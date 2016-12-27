cb53c99e9b7191b69d3a15ecec029e736c701117
andrewlarsen514 <andrew.larse514@gmail.com>
2016-12-26 22:57:24 -0600

    init population

---
+++ tcfj_data_population.sql
@@ -0,0 +1,5 @@
+insert into Recipients (recipient_name, date_started) values ("St.Stephen's Homeless Shelter",CURDATE())
+
+insert into Donors (donar_name, date_started) values ("Seward Co-Op Friendship Store", CURDATE())
+
+insert into Recipients (recipient_name, date_started) values ("Seward Co-Op Friendship Store",CURDATE())
