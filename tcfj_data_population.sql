39e65e89a6e164bf4c1279ee9e9096d5be97f862
Andrew Larsen <andrew.larse514@gmail.com>
2017-01-10 20:47:08 -0600

    Update tcfj_data_population.sql

--- tcfj_data_population.sql
+++ tcfj_data_population.sql
@@ -3,3 +3,23 @@ insert into Recipients (recipient_name, date_started) values ("St.Stephen's Home
 insert into Donors (donar_name, date_started) values ("Seward Co-Op Friendship Store", CURDATE())
 
 insert into Recipients (recipient_name, date_started) values ("Seward Co-Op Friendship Store",CURDATE())
+
+INSERT INTO Shifts
+VALUES
+(
+1,
+"Seward Co-Op Friendship Store",
+"St.Stephen's Homeless Shelter",
+CURDATE(),
+'Andrew Larsen',
+'Alec Larsen',
+'',
+CURDATE(),
+'car',
+100,
+0,
+25,
+'good food',
+'no comment',
+true,
+CURDATE());
