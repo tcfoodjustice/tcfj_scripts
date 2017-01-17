0a159f1cea1c8e4e15437cfb3b38a6066d9c66c0
Andrew Larsen <andrew.larse514@gmail.com>
2017-01-16 22:07:11 -0600

    Update tcfj_table_definitions.sql

--- tcfj_table_definitions.sql
+++ tcfj_table_definitions.sql
@@ -6,7 +6,7 @@ CREATE TABLE Recipients ( recipient_id INT NOT NULL AUTO_INCREMENT, recipient_na
 
 )
 
-CREATE TABLE Shifts( shift_id INT NOT NULL AUTO_INCREMENT, donar_name VARCHAR(100) NOT NULL, recipient_name VARCHAR(100) NOT NULL, rescue_date DATE NOT NULL, volunteer_1 VARCHAR(100), volunteer_2 VARCHAR(100), volunteer_3 VARCHAR(100), pick_up_time DATE, mode_of_transit VARCHAR(25), food_donated_weight INT, food_composted_weight INT, shift_length int, food_type_summary VARCHAR(250), comments VARCHAR(250), supplies_stocked tinyint(1) DEFAULT NULL, submit_time TIMESTAMP, PRIMARY KEY (shift_id),  UNIQUE (rescue_date, donar_name, recipient_name)
+CREATE TABLE Shifts( shift_id INT NOT NULL AUTO_INCREMENT, donar_name VARCHAR(100) NOT NULL, recipient_name VARCHAR(100) NOT NULL, rescue_date DATE NOT NULL, volunteer_1 VARCHAR(100), volunteer_2 VARCHAR(100), volunteer_3 VARCHAR(100), pick_up_time TIMESTAMP, mode_of_transit VARCHAR(25), food_donated_weight INT, food_composted_weight INT, shift_length int, food_type_summary VARCHAR(250), comments VARCHAR(250), supplies_stocked tinyint(1) DEFAULT NULL, submit_time TIMESTAMP, PRIMARY KEY (shift_id),  UNIQUE (rescue_date, donar_name, recipient_name)
 
 );
 
